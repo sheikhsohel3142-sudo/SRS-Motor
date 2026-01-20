41c8c30a901018fe804d09d128f6ddb61552a96d
unknown <sheikhsohel3142@gmail.com>
2026-01-20 14:14:03 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Sahara_Motors_SRS',
+  webDir: 'www'
+};
+
+export default config;
